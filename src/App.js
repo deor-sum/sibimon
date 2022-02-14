1f7fd0219a21b635785ca2b44f96a207ac985931
deorsum <akdeos95@gmail.com>
2022-02-14 21:39:09 +0800

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import NavbarMenu from "./components/NavbarMenu";
+import { Container } from "react-bootstrap";
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <NavbarMenu />
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavbarMenu.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import Logo from "./../assets/logoBimon.png";
+import { FaUser } from "react-icons/fa";
+const NavbarMenu = () => {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        bg="light"
+        sticky="top"
+        expand="md"
+        className="py-4"
+      >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={Logo}
+              width="auto"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Sibimon logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <Nav.Link href="#deets">
+                <FaUser /> Login / Register
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavbarMenu;
